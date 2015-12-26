b0b2192882ac860eb79b92884ed36dbab2e3ca84
mobdev007 <pinkydey233@gmail.com>
2015-12-26 20:54:54 +0530

    LoginApplicationchanges

---
+++ app/src/main/java/com/mycompany/loginapplication/RegistrationActivity.java
@@ -0,0 +1,16 @@
+package com.mycompany.loginapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mine on 12/17/2015.
+ */
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration);
+    }
+}
