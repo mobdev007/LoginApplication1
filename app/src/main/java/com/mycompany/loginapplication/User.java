db744eb80b2a0b7aad20e4fa76c85d81bcaefaeb
mobdev007 <pinkydey233@gmail.com>
2016-01-09 12:02:22 +0530

    User class is added

---
+++ app/src/main/java/com/mycompany/loginapplication/User.java
@@ -0,0 +1,16 @@
+package com.mycompany.loginapplication;
+
+/**
+ * Created by mine on 1/3/2016.
+ */
+public class User {
+    String efname, elname, reuname, repass, eemail;
+    public User (String efname,String elname,String reuname,String repass,String eemail){
+        this.efname= efname;
+        this.elname= elname;
+        this.reuname= reuname;
+        this.repass= repass;
+        this.eemail= eemail;
+    }
+
+}
